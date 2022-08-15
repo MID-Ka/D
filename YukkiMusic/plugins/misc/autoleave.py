9e6ec9f85d81aac5fecf20c6b40c67b587f16de4
MID Ka <111249973+MID-Ka@users.noreply.github.com>
2022-08-15 22:17:05 +0300

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -38,9 +38,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001735223124
+                                and chat_id != -1001472027687
+                                and chat_id != -1001311448721
                             ):
                                 if left == 20:
                                     continue
