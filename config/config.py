6f83239ec3b6c7d1a9bf51dbbc955bd8293b1523
MID Ka <111249973+MID-Ka@users.noreply.github.com>
2022-08-16 02:19:11 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -37,7 +37,7 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001311448721"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001646005687"))
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Music YaFa")
