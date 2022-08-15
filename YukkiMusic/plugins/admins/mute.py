ba13325ce1ef77399c21b8753758f99a3c57be43
MID Ka <111249973+MID-Ka@users.noreply.github.com>
2022-08-15 21:59:23 +0300

    Update mute.py

--- YukkiMusic/plugins/admins/mute.py
+++ YukkiMusic/plugins/admins/mute.py
@@ -12,6 +12,7 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
+from strings.filters import command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import is_muted, mute_on
@@ -22,7 +23,7 @@ MUTE_COMMAND = get_command("MUTE_COMMAND")
 
 
 @app.on_message(
-    filters.command(MUTE_COMMAND)
+    command(MUTE_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
